617cd8dae33960fb46d60f720cd4ab8e8b5ab5de
dwSun <dwSun@Capsule>
2016-09-06 11:18:58 +0800

    initial

---
+++ app.py
@@ -0,0 +1,78 @@
+# Micro gevent chatroom.
+# ----------------------
+# Make things as simple as possible, but not simpler.
+from gevent import monkey; monkey.patch_all()
+from flask import Flask, render_template, request, json
+
+from gevent import queue
+from gevent.pywsgi import WSGIServer
+import pymongo
+from functools import reduce
+
+app = Flask(__name__)
+app.debug = True
+
+conn = pymongo.MongoClient("192.168.1.62",27017)
+db = conn.aqir #连接库
+
+datas ={'msg':'None'}
+
+@app.route('/')
+def choose_name():
+    return render_template('main.html')
+
+@app.route("/put", methods=["POST"])
+def put():
+    message = request.json['message']
+    datas['msg']=message
+    return ''
+
+@app.route("/poll", methods=["POST"])
+def poll():
+    x=list(db.log.find({},{'_id':0}).sort('_id', pymongo.DESCENDING).limit(1))[0]
+    keys = list(x.keys())
+    keys.sort()
+    outData = ''
+    for key in keys:
+        line = ' {0} = {1}<br/>'.format(key,x[key])
+        outData= outData + line
+
+    return json.dumps(outData)
+
+def add(d1,d2):
+    d={}
+    for key in d1:
+        if 'time' not in key:
+            d[key] = d1[key]+ d2[key]
+    return d
+
+@app.route("/poll30s", methods=["POST"])
+def poll30s():
+    return getDat(30)
+
+@app.route("/poll10m", methods=["POST"])
+def poll10m():
+    return getDat(600)
+
+def getDat(time):
+    d=list(db.log.find({},{'_id':0}).sort('_id', pymongo.DESCENDING).limit(time))
+    x=reduce(add,d)
+    for key in x:
+        if 'time' not in key:
+            x[key] = int(x[key]/len(d))
+    x['time'] = d[-1]['time'] +' ~ '+ d[0]['time']
+
+    keys = list(x.keys())
+    keys.sort()
+    outData = ''
+    for key in keys:
+        line = ' {0} = {1}<br/>'.format(key,x[key])
+        outData= outData + line
+    print(len(d))
+
+    return json.dumps(outData)
+
+
+if __name__ == "__main__":
+    http = WSGIServer(('0.0.0.0', 5001), app)
+    http.serve_forever()
---
+++ plantowersersor.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import time
+import datetime
+import requests
+import serial
+import pymongo
+
+
+
+ser = serial.Serial( #下面这些参数根据情况修改
+  port='/dev/ttyUSB0',
+  baudrate=9600,
+  parity=serial.PARITY_NONE,
+  stopbits=serial.STOPBITS_ONE,
+)
+
+import json
+data = ''
+import datetime
+def fun(data):
+    return data.pop(0) * (2**8) + data.pop(0)
+
+while True:
+    conn = pymongo.MongoClient("192.168.1.62",27017)
+    db = conn.aqir #连接库
+    if ser.read() == b'B' and ser.read() == b'M':
+        data = ser.read(2*13 + 2)
+        data = list(map(int,data))
+        par = ser.read(2)
+        print(data)
+        print(par)
+        out = {}
+        out['datalen'] = fun(data)
+        out['pm1-0_std'] = fun(data)
+        out['pm2-5_std'] = fun(data)
+        out['pm10_std'] = fun(data)
+
+        out['pm1-0_atm'] = fun(data)
+        out['pm2-5_atm'] = fun(data)
+        out['pm10_atm'] = fun(data)
+
+        out['dust0-3'] = fun(data)*10000
+        out['dust0-5'] = fun(data)*10000
+        out['dust1-0'] = fun(data)*10000
+        out['dust2-5'] = fun(data)*10000
+        out['dust5-0'] = fun(data)*10000
+        out['dust10'] = fun(data)*10000
+
+        out['dust0-3']=out['dust0-3']-out['dust0-5']
+
+        out['dust0-5']=out['dust0-5']-out['dust1-0']
+
+        out['dust1-0']=out['dust1-0']-out['dust2-5']
+
+        out['dust2-5']=out['dust2-5']-out['dust5-0']
+
+        out['dust5-0']=out['dust5-0']-out['dust10']
+
+
+        out['empty'] = fun(data)
+        out['time'] = str(datetime.datetime.now()).split('.')[0]
+        d=json.dumps(out)
+        db.log.save(out)
